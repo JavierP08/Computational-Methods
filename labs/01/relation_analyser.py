615e2c7d74bb903575d697aadfb0ae4efef1e266
JavierP08 <88795402+JavierP08@users.noreply.github.com>
2023-03-11 22:16:32 -0600

    Update relation_analyser.py

--- labs/01/relation_analyser.py
+++ labs/01/relation_analyser.py
@@ -1,36 +1,51 @@
 import graphviz # https://graphviz.readthedocs.io/en/stable/index.html
 
-def analyze(val):
-    """
-    Here goes your code to do the analysis
-    1. Reflexive: aRa for all a in X,
-    2. Symmetric: aRb implies bRa for all a,b in X
-    3. Transitive: aRb and bRc imply aRc for all a,b,c in X,
-    """
+def analyze(valores, alphabet):
     Reflexive = False
     Symmetric = False
     Transitive = False
 
+    for num in alphabet:
+        if(num, num) in valores:
+            Reflexive = True
+            break
+            
+    for t in valores:
+        if (t[0], t[1]) in valores or t[1] in alphabet or t[0] in alphabet:
+            Symmetric = True
+            break
+            
+    for t0 in valores:
+        for t1 in valores:
+            if (t0[0],t1[1]) in valores:
+                Transitive = True
+                break
+
     return Reflexive,Symmetric,Transitive
 
-def plot():
-    """
-    Here goes your code to do the plot of the set
-    """
-    g = graphviz.Digraph('G', filename='hello.gv')
-    g.edge('Hello', 'World')
+def plot(valores):
+    g = graphviz.Digraph('G', filename='graph.log')
+    for tuple in valores:
+        g.edge(str(tuple[0]),str(tuple[1]))
     g.view()
 
 def main():
     print("Hello World analyzing input!")
-    val = input("Enter your set: ")
-    print(val)
-    Reflexive,Symmetric,Transitive = analyze(val)
+    valores = input("Enter your set: ")
+    
+    print(valores)
+
+    alphabet = {0,1,2,3}
+    valores = valores.replace(' ', '').replace('.', '')
+    t = set(eval(valores))
+    
+    Reflexive,Symmetric,Transitive = analyze(t,alphabet)
+    
     print(f"\
     1. Reflexive: {Reflexive} \
     2. Symmetric: {Symmetric} \
     3. Transitive: {Transitive}")
-    plot()
+    plot(t)
 
 if __name__ == "__main__":
     main()
